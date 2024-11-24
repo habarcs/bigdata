686179aa70b02306b9d014e5eb6d78e87dcfe354
habarcs <>
2024-11-24 22:46:36 +0100

    generate orders and send them to kafka

--- data_gen/__main__.py
+++ data_gen/__main__.py
@@ -5,14 +5,20 @@ from sqlalchemy import create_engine
 
 from data_gen import POSTGRES_CONNECTION
 from data_gen.create_initial_data import create_products, create_retailers
+from data_gen.kafka_producer import event_generation_loop, create_kafka_topics
 
 dirname = os.path.dirname(__file__)
+
 
 def main():
     df = pd.read_csv(dirname + "/data/DataCoSupplyChainDataset.csv", encoding="ISO-8859-1")
     engine = create_engine(POSTGRES_CONNECTION)
+
     create_products(df, engine)
     create_retailers(df, engine)
+
+    create_kafka_topics()
+    event_generation_loop(df, engine)
 
 
 if __name__ == '__main__':
--- data_gen/kafka_producer.py
+++ data_gen/kafka_producer.py
@@ -1,23 +1,18 @@
-import random
-import time
-import uuid
 import json
-import sched
-import signal
+import time
 
-import numpy as np
-import psycopg
-from confluent_kafka.admin import AdminClient, NewTopic
+import pandas as pd
+import sqlalchemy
 from confluent_kafka import Producer
-from faker import Faker
+from confluent_kafka.admin import AdminClient, NewTopic
 
-from data_gen import KAFKA_CONF, POSTGRES_CONNECTION
+from data_gen import KAFKA_CONF
 
 
 def create_kafka_topics():
     admin_client = AdminClient(KAFKA_CONF)
     new_topics = [
-        NewTopic("Orders"), NewTopic("SuppliedMaterial"), NewTopic("Shipping"), NewTopic("ManufacturedProducts")
+        NewTopic("Orders")
     ]
     fs = admin_client.create_topics(new_topics)
     for topic, f in fs.items():
@@ -28,76 +23,6 @@ def create_kafka_topics():
             print(f"Failed to create topic {topic}: {e}")
 
 
-def generate_order_and_shipping_event(fake: Faker):
-    # TODO fix event generation so it is more consistent with data we already have and it makes sense in real world
-    conn: psycopg.Connection
-    with psycopg.connect(POSTGRES_CONNECTION) as conn:
-        cur: psycopg.Cursor
-        with conn.cursor() as cur:
-            customer_ids = [row[0] for row in cur.execute("SELECT CustomerID from Customers").fetchall()]
-            retailer_ids = [row[0] for row in cur.execute("SELECT RetailerID from Retailers").fetchall()]
-            product_ids = [row[0] for row in cur.execute("SELECT ProductID from Products").fetchall()]
-
-    order_id = str(
-        uuid.uuid4())  # we generate an uuid randomly, the chance of collision is very low, see https://en.wikipedia.org/wiki/Universally_unique_identifier#Collisions
-    shipping_options = ["Shipped", "Delivered", "In Transit", "Out for Delivery", "Awaiting Pickup"]
-    payment_options = ["Card", "Gift Card", "Cash", "Cryptocurrency", "Apple Pay", "Google Pay", "Spices", "Gold Coins",
-                       "Klarna"]
-
-    # Mean discount 8 maximum 100 percent, the chance of having a discount is 30 percent
-    discount = max((np.random.poisson(7) + 1), 100) if random.random() < 0.3 else 0
-    customer_id = random.choice(customer_ids)
-    current_time = int(time.time())
-
-    # this part should be more coherent so we can run predictions on it
-    # total amount should be calculated from product prices that have to be queried from database, discount should be
-    # then applied, profit should be the same
-    order = {
-        'OrderID': order_id,
-        'CustomerID': customer_id,
-        'OrderDate': current_time,
-        'TotalAmount': round(random.uniform(5, 100), 2),
-        'OrderStatus': random.choice(shipping_options),
-        'PaymentMethod': random.choice(payment_options),
-        'ProductID': random.sample(product_ids, np.random.poisson(2) + 1),
-        'OrderItemDiscount': discount,
-        'OrderItemTotal': round(random.uniform(1, 1000), 3),
-        'OrderProfitPerOrder': round(random.uniform(0, 1), 3),
-        'OrderQuantity': round(random.uniform(1, 1000)),
-        'OrderCity': fake.city(),
-        'OrderState': fake.state(),
-        'OrderCountry': fake.country(),
-        'ShippingID': order_id,
-        'RetailerID': random.choice(retailer_ids)
-    }
-
-    shippers = ["DHL", "FedEx", "UPS", "USPS (United States Postal Service)", "TNT", "Royal Mail", "Canada Post",
-                "Australia Post", "Maersk", "CMA CGM", "XPO Logistics", "China Post", "Japan Post", "La Poste",
-                "India Post", "Singapore Post (SingPost)", "Aramex", "PostNL", "YRC Worldwide", "Seur"]
-    payment_status = ["Pending", "Paid", "Overdue", "Cancelled", "Refunded", "Failed", "Processing", "Authorized",
-                      "Declined"]
-
-    shipping = {
-        'OrderID': order_id,
-        'ShippingID': order_id,
-        'CustomerID': customer_id,
-        'Address': fake.address(),  # should be queried from database
-        'Shipper': random.choice(shippers),
-        'TrackingNumber': str(uuid.uuid4()),
-        'ShippingDate': current_time,
-        'EstimatedDeliveryDate': current_time + random.randint(3, 10) * 86400,
-        'ActualDeliveryDate': current_time + random.randint(3, 10) * 86400,
-        # should be null if shipment is not yet delivered
-        'ShipmentStatus': random.choice(shipping_options),
-        'Weight': random.uniform(5, 100),
-        'Dimensions': (random.uniform(10, 500), 2),
-        'Payment_status': random.choice(payment_status),
-        'Origin': fake.country(),
-        'Destination': fake.country()
-    }
-    return order, shipping
-
-
 def acked(err, msg):
     if err is not None:
         print("Failed to deliver message: %s: %s" % (str(msg), str(err)))
@@ -105,28 +30,72 @@ def acked(err, msg):
         print("Message produced: %s" % (str(msg)))
 
 
-def gen_and_send_event(fake: Faker, producer: Producer):
-    order, shipping = generate_order_and_shipping_event(fake)
-    key = order["OrderID"]
+def order_generator(df: pd.DataFrame, engine: sqlalchemy.engine.Engine):
+    orders = df[["Type",
+                 "Days for shipping (real)",
+                 "Days for shipment (scheduled)",
+                 "Delivery Status",
+                 "Late_delivery_risk",
+                 "order date (DateOrders)",
+                 "Order Id",
+                 "Product Card Id",
+                 "Order Item Quantity",
+                 "Order Status",
+                 "shipping date (DateOrders)",
+                 "Shipping Mode",
+                 "Customer Id",
+                 ]].rename(columns={
+        "Type": "TransactionType",
+        "Days for shipping (real)": "RealShippingDays",
+        "Days for shipment (scheduled)": "ScheduledShippingDays",
+        "Delivery Status": "DeliveryStatus",
+        "Late_delivery_risk": "LateRisk",
+        "order date (DateOrders)": "OrderData",
+        "Order Id": "OrderID",
+        "Product Card Id": "ProductID",
+        "Order Item Quantity": "ItemQuantity",
+        "Order Status": "Status",
+        "shipping date (DateOrders)": "ShippingData",
+        "Shipping Mode": "ShippingMode",
+        "Customer Id": "CustomerID",
+    })
+    retailers = df[[
+        "Customer City",
+        "Customer State",
+        "Customer Country"
+    ]].rename(columns={
+        "Customer City": "RetailerCity",
+        "Customer State": "RetailerState",
+        "Customer Country": "RetailerCountry"
+    })
+
+    for order, retailer in zip(orders.itertuples(), retailers.itertuples()):
+        with engine.connect() as conn:
+            retailer_id = conn.execute(sqlalchemy.text(
+                'SELECT "RetailerID" from "Retailers" '
+                'WHERE "RetailerCity" LIKE :retailer_city '
+                'AND "RetailerState" LIKE :retailer_state '
+                'AND "RetailerCountry" LIKE :retailer_country'),
+                {
+                    "retailer_city": retailer.RetailerCity,
+                    "retailer_state": retailer.RetailerState,
+                    "retailer_country": retailer.RetailerCountry
+                }).fetchone()[0]
+            order_dict = order._asdict()
+            order_dict["RetailerID"] = retailer_id
+            del order_dict["Index"]
+        yield order_dict
+
+
+def send_event(producer: Producer, order: dict):
+    key = str(order["OrderID"])
+    producer.poll(0)
     producer.produce("Orders", key=key, value=json.dumps(order), callback=acked)
-    producer.produce("Shipping", key=key, value=json.dumps(shipping), callback=acked)
-    producer.poll(1)
 
 
-def event_generation_loop(fake: Faker):
+def event_generation_loop(df: pd.DataFrame, engine: sqlalchemy.engine.Engine):
     producer = Producer(KAFKA_CONF)
-    scheduler = sched.scheduler(time.time, time.sleep)
-
-    def sigterm_handler(_, __):
-        print("Terminated: cancelling events")
-        for event in scheduler.queue:
-            scheduler.cancel(event)
-        producer.flush()
-
-    def repeat_task():
-        scheduler.enter(2, 1, gen_and_send_event, (fake, producer))
-        scheduler.enter(2, 1, repeat_task, ())
-
-    signal.signal(signal.SIGTERM, sigterm_handler)
-    repeat_task()
-    scheduler.run()
+    for order in order_generator(df, engine):
+        send_event(producer, order)
+        time.sleep(1)
+    producer.flush()
