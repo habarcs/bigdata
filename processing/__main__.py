ae818949d40401e519e52dde2aa893b47788735e
redrobot279 <ricardo.red279@gmail.com>
2024-12-07 16:28:13 +0100

    fix some indentation errors

--- processing/__main__.py
+++ processing/__main__.py
@@ -59,73 +59,68 @@
 #         )   
 #     """)
 
-from pyflink.table import EnvironmentSettings, TableEnvironment, TableDescriptor, Schema, DataTypes
+from pyflink.table import EnvironmentSettings, TableEnvironment, TableDescriptor, Schema, DataTypes, FormatDescriptor
 
 def main():
-    
-# Create a streaming TableEnvironment
+    # Create a streaming TableEnvironment
     env_settings = EnvironmentSettings.in_streaming_mode()
     table_env = TableEnvironment.create(env_settings)
 
     # Define Kafka table schema
-    kafka_schema = Schema.new_builder()
-    .column("TransactionType", DataTypes.STRING()) 
-    .column("RealShippingDays", DataTypes.INT()) 
-    .column("ScheduledShippingDays", DataTypes.INT()) 
-    .column("DeliveryStatus", DataTypes.STRING()) 
-    .column("LateRisk", DataTypes.INT()) 
-    .column("OrderData", DataTypes.TIMESTAMP()) 
-    .column("OrderID", DataTypes.INT()) 
-    .column("ProductID", DataTypes.INT()) 
-    .column("ItemQuantity", DataTypes.INT())
-    .column("Status", DataTypes.STRING()) 
-    .column("ShippingData", DataTypes.TIMESTAMP()) 
-    .column("ShippingMode", DataTypes.STRING()) 
-    .column("CustomerID", DataTypes.INT()) 
-    .column("RetailerID", DataTypes.INT()) 
-    .build()
+    kafka_schema = (
+        Schema.new_builder()
+            .column("TransactionType", DataTypes.STRING())
+            .column("RealShippingDays", DataTypes.INT())
+            .column("ScheduledShippingDays", DataTypes.INT())
+            .column("DeliveryStatus", DataTypes.STRING())
+            .column("LateRisk", DataTypes.INT())
+            .column("OrderData", DataTypes.TIMESTAMP())
+            .column("OrderID", DataTypes.INT())
+            .column("ProductID", DataTypes.INT())
+            .column("ItemQuantity", DataTypes.INT())
+            .column("Status", DataTypes.STRING())
+            .column("ShippingData", DataTypes.TIMESTAMP())
+            .column("ShippingMode", DataTypes.STRING())
+            .column("CustomerID", DataTypes.INT())
+            .column("RetailerID", DataTypes.INT())
+            .build()
+    )
 
     # Create Kafka source table
     table_env.create_temporary_table(
-    'kafka_orders',
-    TableDescriptor.for_connector('kafka')
-    .schema(kafka_schema)
-    .option("topic", "Orders")  # Kafka topic
-    .option("properties.bootstrap.servers", "kafka:9092")  # Kafka broker
-    .option("properties.group.id", "flink_group")  # Consumer group
-    .option("scan.startup.mode", "earliest-offset")  # Start reading from earliest offset
-    .format(
-        FormatDescriptor.for_format("json")
-        .option("timestamp-format.standard", "ISO-8601")  # Timestamp format
-        .build()
-    )
-    .build()
+        'kafka_orders',
+        TableDescriptor.for_connector('kafka')
+            .schema(kafka_schema)
+            .option("topic", "Orders")  # Kafka topic
+            .option("properties.bootstrap.servers", "kafka:9092")  # Kafka broker
+            .option("properties.group.id", "flink_group")  # Consumer group
+            .option("scan.startup.mode", "earliest-offset")  # Start reading from earliest offset
+            .format(
+                FormatDescriptor.for_format("json")
+                    .option("timestamp-format.standard", "ISO-8601")  # Timestamp format
+                    .build()
+            )
+            .build()
     )
 
-    # Define a sink table (for testing, using print connector) I guess this will be the transactions table?
+    # Define a sink table (using print connector for testing)
     table_env.execute_sql("""
     CREATE TABLE print_sink (
         OrderID INT,
         ProductID INT,
-        ItemQuantity INT, 
-        RetailerID INT 
+        ItemQuantity INT,
+        RetailerID INT
     ) WITH (
-        'connector' = 'print'  
+        'connector' = 'print'
     )
     """)
 
     # Query Kafka table and write to sink
     table_env.execute_sql("""
     INSERT INTO print_sink
-    SELECT *
+    SELECT OrderID, ProductID, ItemQuantity, RetailerID
     FROM kafka_orders
     """)
-
-
-
-
-
-
 
 if __name__ == '__main__':
     main()
