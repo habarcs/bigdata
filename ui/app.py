638894453c30ebfb84d033d1133421c29125a273
habarcs <>
2025-01-10 12:13:15 +0100

    containerize ui

--- ui/app.py
+++ ui/app.py
@@ -1,28 +1,27 @@
-from flask import Flask, render_template, request, jsonify
 import psycopg
+from flask import Flask, render_template, request, jsonify
 
-POSTGRES_CONNECTION = "host=localhost dbname=postgres user=postgres password=supersecret port=5432"
+POSTGRES_CONNECTION = "host=sql-database dbname=postgres user=postgres password=supersecret port=5432"
 
 app = Flask(__name__)
 
-def get_db_connection():
-    return psycopg.connect(POSTGRES_CONNECTION, autocommit=True)
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/inventory', methods=['GET', 'POST'])
 def inventory():
     if request.method == 'GET':
-        conn = get_db_connection()
-        with conn.cursor() as cur:
-            cur.execute("SELECT retailer_id, retailer_name FROM retailers")
-            retailers = [{'id': row[0], 'name': row[1]} for row in cur.fetchall()]
-            cur.execute("SELECT product_id, product_name FROM products")
-            products = [{'id': row[0], 'name': row[1]} for row in cur.fetchall()]
-        return render_template('inventory.html', retailers=retailers, products=products)
-    
+        with psycopg.connect(POSTGRES_CONNECTION, autocommit=True) as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT retailer_id, retailer_name FROM retailers")
+                retailers = [{'id': row[0], 'name': row[1]} for row in cur.fetchall()]
+                cur.execute("SELECT product_id, product_name FROM products")
+                products = [{'id': row[0], 'name': row[1]} for row in cur.fetchall()]
+            return render_template('inventory.html', retailers=retailers, products=products)
+
     elif request.method == 'POST':
         filters = request.json
         retailer_ids = filters.get('retailers', [])
@@ -49,22 +48,24 @@ def inventory():
                 AND (%s::int[] IS NULL OR p.product_id = ANY(%s))
         """
 
-        conn = get_db_connection()
-        with conn.cursor() as cur:
-            cur.execute(query, (retailer_ids, retailer_ids, product_ids, product_ids))
-            data = [
-                {
-                    'retailer_name': row[0],
-                    'product_name': row[1],
-                    'quantity_on_hand': row[2],
-                    'status': row[3]
-                } for row in cur.fetchall()
-            ]
-        return jsonify(data)
+        with psycopg.connect(POSTGRES_CONNECTION, autocommit=True) as conn:
+            with conn.cursor() as cur:
+                cur.execute(query, (retailer_ids, retailer_ids, product_ids, product_ids))
+                data = [
+                    {
+                        'retailer_name': row[0],
+                        'product_name': row[1],
+                        'quantity_on_hand': row[2],
+                        'status': row[3]
+                    } for row in cur.fetchall()
+                ]
+            return jsonify(data)
+
 
 @app.route('/forecasting')
 def forecasting():
     return render_template('forecasting.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
