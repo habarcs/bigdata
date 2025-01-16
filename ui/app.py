ceda811efd487698c7306e2eb6c8451e27d2538e
strdubtseva <starodubtceva.mar@gmail.com>
2025-01-16 15:14:29 +0100

    initial forecasting ui

--- ui/app.py
+++ ui/app.py
@@ -1,7 +1,9 @@
 import psycopg
+import plotly.express as px
+import pandas as pd
 from flask import Flask, render_template, request, jsonify
 
-POSTGRES_CONNECTION = "host=sql-database dbname=postgres user=postgres password=supersecret port=5432"
+POSTGRES_CONNECTION = "host=localhost dbname=postgres user=postgres password=supersecret port=5432"
 
 app = Flask(__name__)
 
@@ -16,11 +18,40 @@ def inventory():
     if request.method == 'GET':
         with psycopg.connect(POSTGRES_CONNECTION, autocommit=True) as conn:
             with conn.cursor() as cur:
+                # Fetch retailers and products
                 cur.execute("SELECT retailer_id, retailer_name FROM retailers")
                 retailers = [{'id': row[0], 'name': row[1]} for row in cur.fetchall()]
                 cur.execute("SELECT product_id, product_name FROM products")
                 products = [{'id': row[0], 'name': row[1]} for row in cur.fetchall()]
-            return render_template('inventory.html', retailers=retailers, products=products)
+                
+                # Fetch the first 10 inventory items
+                cur.execute("""
+                    SELECT 
+                        r.retailer_name,
+                        p.product_name,
+                        i.quantity_on_hand,
+                        CASE 
+                            WHEN i.quantity_on_hand <= 0 THEN 'Out of Stock'
+                            WHEN i.quantity_on_hand <= i.reorder_level THEN 'Low Stock'
+                            ELSE 'In Stock'
+                        END AS status
+                    FROM 
+                        inventory i
+                    JOIN 
+                        retailers r ON i.retailer_id = r.retailer_id
+                    JOIN 
+                        products p ON i.product_id = p.product_id
+                    LIMIT 10
+                """)
+                inventory_data = [
+                    {
+                        'retailer_name': row[0],
+                        'product_name': row[1],
+                        'quantity_on_hand': row[2],
+                        'status': row[3]
+                    } for row in cur.fetchall()
+                ]
+            return render_template('inventory.html', retailers=retailers, products=products, inventory_data=inventory_data)
 
     elif request.method == 'POST':
         filters = request.json
@@ -62,9 +93,47 @@ def inventory():
             return jsonify(data)
 
 
-@app.route('/forecasting')
+
+@app.route('/forecasting', methods=['GET', 'POST'])
 def forecasting():
-    return render_template('forecasting.html')
+    with psycopg.connect(POSTGRES_CONNECTION, autocommit=True) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT retailer_id FROM forecast")
+            retailers = [{'id': row[0]} for row in cur.fetchall()]
+            cur.execute("SELECT product_id FROM forecast")
+            products = [{'id': row[0]} for row in cur.fetchall()]
+
+    if request.method == 'GET':
+        return render_template('forecasting.html', retailers=retailers, products=products)
+
+    elif request.method == 'POST':
+        # Fetch selected retailer and product IDs from the form
+        retailer_id = request.form['retailer_id']
+        product_id = request.form['product_id']
+
+        # Query the forecast table for the selected retailer and product
+        with psycopg.connect(POSTGRES_CONNECTION, autocommit=True) as conn:
+            with conn.cursor() as cur:
+                cur.execute("""
+                    SELECT ds, item_quantity
+                    FROM forecast
+                    WHERE retailer_id = %s AND product_id = %s
+                    ORDER BY ds
+                """, (retailer_id, product_id))
+                data = cur.fetchall()
+
+        # Convert the data to a Pandas DataFrame
+        df = pd.DataFrame(data, columns=['ds', 'item_quantity'])
+
+        # Generate the interactive plot
+        fig = px.line(df, x='ds', y='item_quantity', 
+                      title=f'Demand Forecast for Product <b>{product_id}</b> at Retailer <b>{retailer_id}</b>',
+                      labels={'ds': 'Date', 'item_quantity': 'Predicted Demand'},
+                      template='plotly_white')
+        graph_html = fig.to_html(full_html=False)
+
+        # Return the plot in the response
+        return render_template('forecasting.html', graph_html=graph_html, retailers=retailers, products=products)
 
 
 if __name__ == '__main__':
